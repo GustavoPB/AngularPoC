452c5b077d5546a9894c2054be4167c2107b7186
Puig Baeza <gustavo.puig.baeza@accenture.com>
2016-06-28 11:19:54 +0200

    Scaffolding inicial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scaAngularLightPoCApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scaAngularLightPoCApp
+ */
+angular.module('scaAngularLightPoCApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
